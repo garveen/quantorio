c527c6ca86e06f9d9e5b3268c4ef680d43821451
Gavin <acabin@live.com>
2018-02-08 08:17:10 +0800

    ready for build

--- config/index.js
+++ config/index.js
@@ -50,12 +50,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../public'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/public',
 
     /**
      * Source Maps
