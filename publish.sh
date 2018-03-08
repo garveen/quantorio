b154378b74ef794f3f21b66fd82d07e2a5e04f70
Gavin <acabin@live.com>
2018-03-09 05:31:22 +0800

    publish

--- publish.sh
+++ publish.sh
@@ -3,8 +3,9 @@ git checkout -b tmp-publish
 git reset --hard master
 npm run build
 git add public -f
-git commit -m 'assets'
-git tag $1
+git commit -m 'publish'
+git subtree split --prefix public -b gh-pages
+git push origin gh-pages --force
 git checkout master
 git branch -D tmp-publish
 npm run parse
