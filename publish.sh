983c62811ea86e1b32f3efc990391b0c2ec32d9e
Gavin <acabin@live.com>
2018-03-09 16:41:48 +0800

    publish script

--- publish.sh
+++ publish.sh
@@ -4,8 +4,9 @@ git checkout -b tmp-publish
 npm run build
 git add public -f
 git commit -m 'publish'
-git subtree split --prefix public -b gh-pages
-git push origin gh-pages --force
+git subtree split --prefix public -b tmp-publish-gh-pages
+git push origin tmp-publish-gh-pages:gh-pages --force
 git checkout master
 git branch -D tmp-publish
+git branch -D tmp-publish-gh-pages
 npm run parse
